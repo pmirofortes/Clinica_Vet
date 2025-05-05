52269b7c9ed022ea993d7eb6a0324132cfc86c8c
Pau Miró <463104.joan23@fje.edu>
2025-05-05 13:58:48 +0200

    estructura de archivos creados, falta manel que nos pase el sql

---
+++ fomularios/registro_nueva_mascota.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        <label for="nombre">Nombre de la mascota:</label><br>
+        <input type="text" id="nombre" name="nombre"><br><br>
+        
+        <label for="tipo">Tipo de mascota:</label><br>
+        <input type="text" id="tipo" name="tipo"><br><br>
+        
+        <label for="raza">Raza:</label><br>
+        <input type="text" id="raza" name="raza"><br><br>
+        
+        <label for="edad">Edad:</label><br>
+        <input type="number" id="edad" name="edad"><br><br>
+
+        <label for="peso">Peso:</label><br>
+        <input type="number" id="peso" name="peso"><br><br>
+        
+        <label for="color">Color:</label><br>
+        <input type="text" id="color" name="color"><br><br>
+
+        <label for="raza">Dueño:</label><br>
+        <input type="text" id="raza" name="raza"><br><br>
+        <!-- introducir DNI y que lo busque en la base de datos, sino esta que salga un mensaje para redirigir a registrar propietario -->
+        <input type="submit" value="Registrar Mascota">
+
+    </form> 
+</body>
+</html>
